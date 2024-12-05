a9752faf1a80013f17c5bc2436c1cd36f672ddb5
김은지 <univ217@naver.com>
2024-12-05 14:13:19 +0900

    파이어베이스 + C파트 연동

--- lib/LogIn.dart
+++ lib/LogIn.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'SignUp.dart'; // SignUpScreen을 가져옵니다.
+import 'SignUp.dart';
+import 'navigation_bar.dart'; // SignUpScreen을 가져옵니다.
 
 void main() => runApp(LogIn());
 
@@ -63,7 +64,9 @@ class _LoginPageState extends State<LoginPage> {
         SnackBar(content: Text('로그인 성공!')),
       );
       // 로그인 성공 후 다음 화면으로 이동
-      // Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => NextPage()));
+      Navigator.pushReplacement(context,
+        MaterialPageRoute(builder: (context) => NavigationMenu()),
+      );
     } on FirebaseAuthException catch (e) {
       String message;
       if (e.code == 'user-not-found') {
